83d192ce63d7398b193f8344fb70e2aa55a8dbef
Simeon Akanbi <simeonakanbi99@gmail.com>
2025-02-17 22:56:03 +0100

    Enhance congratulatory page with Lottie animation and improved styling

--- pages/congrat.js
+++ pages/congrat.js
@@ -1,14 +1,16 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import styles from '../assets/css/styles.module.css';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import styles from "../assets/css/styles.module.css";
+import lottieCongrats from './../assets/lottie/completed.json';
 import React, { useEffect, useState } from "react";
+import { Player } from '@lottiefiles/react-lottie-player';
 import PouchDB from "pouchdb";
 
 const db = new PouchDB("rcf_database");
 
 const Results = () => {
   const router = useRouter();
-  const [ipAddress, setIpAddress] = useState('');
+  const [ipAddress, setIpAddress] = useState("");
 
   useEffect(() => {
     // Extract IP address from the URL in the browser
@@ -55,8 +57,12 @@ const Results = () => {
       // Loop through each entry and save the score
       for (const entry of dataEntries) {
         // Construct the URL with parameters using the retrieved IP address
-        const url = `http://${ipAddress}/rcf_db/save_score.php?username=${encodeURIComponent(entry.username)}&id=${encodeURIComponent(entry.id)}&score=${encodeURIComponent(btoa(entry.integerValue))}`;
-        
+        const url = `http://${ipAddress}/rcf_db/save_score.php?username=${encodeURIComponent(
+          entry.username
+        )}&id=${encodeURIComponent(entry.id)}&score=${encodeURIComponent(
+          btoa(entry.integerValue)
+        )}`;
+
         // Make a request to the endpoint
         const response = await fetch(url, {
           method: "POST", // or "GET" depending on your server setup
@@ -64,59 +70,90 @@ const Results = () => {
         });
 
         // Handle response as needed
-        console.log("Score saved successfully for", entry.username, ":", response);
+        console.log(
+          "Score saved successfully for",
+          entry.username,
+          ":",
+          response
+        );
       }
 
       // Redirect to login.js after saving all scores
-      router.push('/login');
+      router.push("/login");
     } catch (error) {
       console.error("Error saving scores:", error);
     }
   };
 
   return (
-    <>     
-      <div className={styles.body}>
-			<Head>
-			<link rel="icon" href="rcf_logo_172_X_155.png" />
-			<title>Congratulations</title>
-		</Head>
+    <>
+      <div className={styles.congrats} >
+        <Head>
+          <link rel="icon" href="rcf_logo_172_X_155.png" />
+          <title>CONGRATULATIONS</title>
+        </Head>
 
-      <div className={styles.header}>
-      <img className={styles.logo} onClick={() => window.location.href = "quizphy113"} src="rcf_logo_172_X_155.png" alt="RCF_Logo" width="48" height="48"/>
-		<div className={styles.user_info}>
-			<div className={styles.timer}>Elapsed
-			{/* <CountdownTimer/> */}
-      {/* <p>status</p> */}
-      {/* <p>{autoSubmitted}</p> */}
-			</div>
-			<img className={styles.ImgCircle} src="avatar.png" alt="img" width="57" height="51"/>
-			<div>
+        <div className={styles.congHeader}>
+          <img
+            className={styles.logo}
+            onClick={() => (window.location.href = "quizphy113")}
+            src="rcf_logo_172_X_155.png"
+            alt="RCF_Logo"
+            width="70"
+            // height="48"
+          />
+          {/* <div className={styles.user_info}> */}
+            {/* <div className={styles.timer}>Elapsed */}
+            {/* <CountdownTimer/> */}
+            {/* <p>status</p> */}
+            {/* <p>{autoSubmitted}</p> */}
+            {/* </div> */}
+            {/* <img className={styles.ImgCircle} src="avatar.png" alt="img" width="57" height="51"/> */}
+            {/* <div>
 				<span className={styles.userid}>ID</span><br/>
 				<span className={styles.user}>Name</span>
-			</div>
-		</div>
-      </div>
+			</div> */}
+          {/* </div> */}
+        </div>
 
-	 <div className={styles.congframe}>
-		<div className={styles.xmage}>
-			<img style={{width: '200px', height: '100px', align: 'center', top: '50%',	left: '50%', margin:'0px auto'}} src="shine.png" alt="Congratulation" />
-		</div>
-		<div style={{
-		  fontFamily: 'Satoshi',
-		  fontStyle: 'normal',
-		  fontWeight: 500,
-		  fontSize: '24px',
-		  lineHeight: '32px',
-		  textAlign: 'center',
-		  padding: '20px',
-		  borderRadius: '4px'
-		}}>Congratulation on the successful completion of your quiz</div>
-    <button className={styles.loginbtn} onClick={saveAllScores}>Close</button>
-    {/* <button className={styles.loginbtn} onClick={wipeDatabase}>Wipe Database</button> */}
-    <div style={{color:'red', top:'20px'}}>Ensure you click close before leaving</div>
-	</div>
-    </div>
+        <div className={styles.congframe}>
+        <Player
+            autoplay
+            loop
+            src={lottieCongrats}
+            style={{ height: '150px', width: '150px' }} />
+        
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          textAlign: 'center',
+          flexDirection: 'column'
+        }}>
+          <div style={{
+            paddingBlock: '10px',
+            fontSize: "30px",
+            fontWeight: 600,
+            fontFamily: ""
+          }}>
+            WELL DONE!
+          </div>
+          <div style={{
+            paddingBlock: '10px',
+          }}>
+            You've successfully completed your quiz.
+          </div>
+        </div>
+
+        <div style={{ color: "red", marginTop: "20px" }}>
+          Make sure to click <b>Close</b> before leaving.
+        </div>
+        
+        <button className={styles.loginbtn} onClick={saveAllScores}>
+          Close
+        </button>
+          {/* <button className={styles.loginbtn} onClick={wipeDatabase}>Wipe Database</button> */}
+        </div>
+      </div>
 
       {dataEntries.map((entry) => (
         <div key={entry._id}>
@@ -132,13 +169,6 @@ const Results = () => {
 };
 
 export default Results;
-
-
-
-
-
-
-
 
 // import Head from 'next/head';
 // import { useRouter } from 'next/router';
@@ -182,7 +212,7 @@ export default Results;
 //       for (const entry of entries) {
 //         // Construct the URL with parameters using the retrieved IP address
 //         const url = `http://${ipAddress}/rcf_db/save_score.php?username=${encodeURIComponent(entry.username)}&id=${encodeURIComponent(entry.id)}&score=${encodeURIComponent(entry.integerValue)}`;
-        
+
 //         // Make a request to the endpoint
 //         const response = await fetch(url, {
 //           method: "POST", // or "GET" depending on your server setup
@@ -218,7 +248,7 @@ export default Results;
 //   }
 
 //   return (
-//     <>     
+//     <>
 //       <div className={styles.body}>
 //         <Head>
 //           <link rel="icon" href="rcf_logo_172_X_155.png" />
@@ -271,4 +301,3 @@ export default Results;
 // };
 
 // export default Results;
-
